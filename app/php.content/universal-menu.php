0e15f16187719dce81951d633bd183c6461aac64
oiramS <oiram.1096.siul@gmail.com>
2020-06-26 00:35:38 -0500

    submenu-admin

--- app/php.content/universal-menu.php
+++ app/php.content/universal-menu.php
@@ -7,41 +7,22 @@
     -->
     <br><a href="menu.php?id=inicio">Inicio</a><br>
     <a href="rsvp.php?id=rsvp">Reservar Habitación</a><br>
-    <?php 
-        $isReceptionist = FALSE;
-        if(!$isReceptionist)
-            echo '<br><br><br><br><br><br><br><br><br><br>';
-        else{
-            echo'
-            <a href="check-in.php?id=in">Check-In</a><br>
-            <a href="check-out.php?id=out">Check-Out</a><br>
-            <a href="#?id=adsrv">Mantenimiento</a><br>
-                ';
-        }
-    ?>
-    
+    <a href="check-in.php?id=in">Check-In</a><br>
+    <a href="check-out.php?id=out">Check-Out</a><br>
+|   <a href="#?id=adsrv">Mantenimiento</a><br>
     <div class="dropup">
         <a href="">Servicios Adicionales</a><br>
         <div class="dropup-content">
             <br><a href="#">Gimnasio</a><br>
             <a href="#">Yoga</a><br>
             <a href="#">Zumba</a><br>
-            <?php 
-                $isReceptionist = FALSE;
-                if(!$isReceptionist)
-                    echo '';
-                else{
-                    echo'
-                    <a href="#">Mantenimiento Adicional</a><br>
-                        ';
-                }
-            ?>
+            <a href="#">Mantenimiento Adicional</a><br>
             <a href="#">Información de las Habitaciones </a><br>
         </div>
     </div>
     <?php
-        $isAdmin = FALSE;
-        if(!$isAdmin)
+        $tipoEmpleado = TRUE;
+        if(!$tipoEmpleado)
             echo ''; 
         else
            echo  '
